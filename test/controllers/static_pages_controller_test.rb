14f126776e629a2d91635baa8a35a86c4bd691d6
Canaan <cjmckenz@uvm.edu>
2022-03-30 14:11:12 -0400

    contact rerouting

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,19 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help"
   end
 
   test "should get about" do 
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About"
   end
@@ -22,5 +22,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do 
     get root_url
     assert_response :success
+  end
+
+  test "should get contact" do 
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact"
   end
 end
