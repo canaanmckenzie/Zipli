8988cf3d3e42d72d8dd72b102314df6b6091b73a
Canaan <cjmckenz@uvm.edu>
2022-04-15 10:30:06 -0400

    add minimal password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.{1}[a-z]+\z/i
 	validates(:email, presence: true, length:{maximum: 255},
 		format:{with: VALID_EMAIL_REGEX}, uniqueness: true)
+	validates(:password, presence:true, length: {minimum: 6})
 
 	has_secure_password 
 end
