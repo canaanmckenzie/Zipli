bd0bd4b3b6310705bbc6b2ae652d6194a6e0f5af
Canaan <cjmckenz@uvm.edu>
2022-04-25 11:03:28 -0400

    checking out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
 
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do 
+      post users_path params: {user: {name: "Rails Tutorial",
+                                      email: "example@railstutorial.org",
+                                      password: "foobar66",
+                                      password_confirmation:"foobar66"}}      
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
